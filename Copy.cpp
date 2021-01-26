208bc0f68e561cd36129fc335abca39749f07371
Stragga <70168490+Stragga@users.noreply.github.com>
2021-01-26 21:28:14 +0200

    Update Copy.cpp
    new

--- Copy.cpp
+++ Copy.cpp
@@ -15,3 +15,29 @@ void Copy(const tstring& old_path, const tstring& new_path, bool move);
 void print(LPCTSTR fmt, ...);
 
 
+int _tmain(int argc, LPTSTR argv[]) {
+    if (argc > 2) {
+        Copy(argv[1], argv[2], false);
+
+        Copy(argv[1], argv[2], true);
+    }
+    return 0;
+}
+
+
+bool Exists(const tstring& fp) {
+    DWORD fg = GetFileAttributes(fp.c_str());
+    if ((fg == DWORD(~0)) && (GetLastError() & ERROR_FILE_NOT_FOUND))
+        return false;
+    return (fg & FILE_ATTRIBUTE_DIRECTORY) ? false : true;
+}
+
+int Help(void) {
+    TCHAR s[] = _T("1.Перезаписать? 2.Пропустить? 3.Перезаписывать для всех? 4.Отмена?");
+    CharToOem(s, s);
+    _putts(s);
+    int n = 0;
+    _tscanf(_T("%d"), &n);
+    return n;
+}
+
